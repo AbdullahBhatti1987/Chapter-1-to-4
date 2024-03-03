cd8e21d872656679eec59eab9b3ff10f207129e4
AbdullahBhatti1987 <abdullah.bhatti345@gmail.com>
2024-03-04 03:43:04 +0500

    JavaScript assignment done
    JavaScript assignment done Chapter-01 to Chapter-04

---
+++ app.js
@@ -0,0 +1,118 @@
+// ========================Chapter 01===========================
+// Queston No. 01
+// Queston No. 02
+var valid_Password = "Error! Please enter a valid password.";
+alert(valid_Password);
+// Queston No. 03
+var welCome = "Welcome to JS Land...";
+var coding = "Happy Coding!";
+alert(welCome + " \n " + coding);
+// Queston No. 04
+alert(welCome);
+var dialog = "Prevent this page from additional dialogs.";
+alert(coding + " \n " + dialog );
+// Queston No. 05
+console.log("Hello...I can run JS through my web browser's console")
+// Queston No. 06
+// done
+// Queston No. 07
+/* <pre style="border: 1px solid black; width: 35%;">
+following sections of your project in exercise 6:
+a. Head
+b. Body (before your page’s HTML)
+<mark>c. Body (inside your page’s HTML)</mark>
+d. Body (after your page’s HTML)
+</pre> */
+
+
+
+
+// ========================Chapter 02===========================
+
+// Question No. 01
+var userName = "Abdullah Bhatti";
+// Question No. 02
+var myName = userName;
+// Question No. 03
+var message;
+message = "Hello World"
+alert(message);
+// Question No. 04
+var student_Name = "Abdullah Bhatti";
+var student_Age = "37";
+var certification = "Certificate Mobile Application Development"
+alert(student_Name);
+alert(student_Age + " years old");
+alert(certification);
+// Question No. 05
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(pizza);
+// Question No. 06
+var email = "abdullah.bhatti345@gmail.com"
+alert("My email address is " + email);
+// Question No. 07
+var book = "A smart way to learn JavaScript.";
+alert("I am trying to learn from the book " + book);
+// Question No. 08
+document.write("Yah! I can write HTML content through JavaScript");
+// Question No. 09
+var dummy_String = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert(dummy_String);
+document.write("</br>" + dummy_String);
+
+
+// ========================Chapter 03===========================
+
+//Question No. 01
+// number variable already declare
+alert("I am " + student_Age + " year old");
+//Question No. 02
+var visitors = 104;
+alert("You have visisted this site " + visitors + " times");
+//Question No. 03
+var dob = 1987;
+document.write("<br> My birth year is " + dob);
+document.write("<br> Date type of my declare variable is number")
+//Question No. 04
+var visitor_Name = "Abdullah Bhatti";
+var product_Title = "T-Shirt";
+var purchase_quantity = 5;
+document.write("<br>" + visitor_Name + " ordered " + purchase_quantity + product_Title + " (s) on XYZ Clothing store");
+
+// ========================Chapter 04===========================
+
+//Question No. 01
+var first_Name = "Abdullah", last_Name = "Bhatti", full_Name = first_Name + " " + last_Name;
+//Question No. 02
+
+// Legal variable name
+
+var customer_Number = 10025;
+var customer_Name = "Siraj";
+var customer_Dob = 1978;
+var customer_City = "Karachi";
+var customer_Status = "Active";
+
+// Illegal variable name
+
+// Temporary Comment
+
+// var Customer_# = 10023;
+// var Customer_Name = "Farooq";
+// var DateofBirth = 1988;
+// var @_city = "Karachi";
+// var Customer-Status = "Block";
+
+//Question No. 03
+
+var rules = "Rules for naming JS variables";
+document.write("<h1> " + rules + " </h1>");
+var numbers = "numbers";
+var $sign = "$";
+var _sign = "_";
+document.write("<br> Variable names can only contain" + numbers+ "," + $sign + "," + " and " + _sign + "." + " For example : " + $sign + "my" + _sign + "$my_1stVariable" );
+document.write("<br> Variables must begin with a " + $sign+ " or " + _sign + " For example : " + $sign + "name" + "," + _sign + "name or name" );
+var variable_Name = "sensitive";
+document.write("<br> Variable names are case " + variable_Name);
+var js_Keywords = "keywords";
+document.write("<br> Variable names should not be JS " + js_Keywords);
